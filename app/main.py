81d25af93790386b3ff79b3a09c008ad50a12194
kacienk <ktcienkosz@gmail.com>
2024-05-15 08:52:44 +0200

    add initial data to empty db

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.database import db
 from app.endpoints.nodes import router as nodes_router
-
+from app.models import NodeModel
+from app.utils import NodeType
 
 app = FastAPI()
 
@@ -15,5 +19,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.on_event("startup")
+async def init_db():
+    nodes = db.get_collection("nodes")
+    element = await nodes.find_one()
+    if not element:
+        node = NodeModel(
+            name="root",
+            node_type=NodeType.DIR,
+            data="",
+            parent_id=None
+        )
+        await nodes.insert_many([node.model_dump(by_alias=True, exclude={"id"})])
+
 
 app.include_router(nodes_router, prefix="/api/v1")
